8f5f5a716a552c0d9cfe2cc740943e3b1b573ffa
Daffa Shelby <98572099+DafaAF@users.noreply.github.com>
2022-08-26 09:07:48 +0700

    assalamualaikum

---
+++ PBO1/index.php
@@ -0,0 +1,17 @@
+<?php
+ include"PBO.php";
+$koneksi1 = new koneksi();
+$db =$koneksi1->getkoneksi();   
+ $query = $db->query("select * from calon_siswa");
+
+ while ($data = $query->fetch() ) :?>
+    <p>
+        <a href="index_edit.php?id=<?= $data['id']; ?>">
+            <?= $data['nama'] ?>
+        </a>
+        
+        <a href="proses_hapus.php?id=<?= $data['id']; ?>" style="color:white;background:red;padding:2px;border-radius:3px">
+            Hapus
+        </a>
+    </p>
+<?php endwhile ?>
